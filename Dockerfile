2aef60a3137015193e8c5f110544a8585ea104b8
thathy23 <thathy23@gmail.com>
2023-04-24 00:37:41 -0300

    retirando comentarios

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,3 @@
-#FROM node:alpine
-
-#WORKDIR / user/app
-
-#COPY package*.json ./
-#RUN npm install
-
-#COPY . .
-
-#EXPOSE 3000
-
-#CMD 
-
-
 FROM node:14
 
 WORKDIR /.
--- index.js
+++ index.js
@@ -3,9 +3,3 @@ var http = require("http");
 http.createServer(function(req, res){
     res.end("Hello Sistemas para Internet - P3 - Noite. A melhor turma.");
 }).listen(3000);
-
-
-
-
-
-
